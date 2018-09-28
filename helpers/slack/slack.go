3e46189e95a80f4276ebf8dea8a47d4989472723
Kristinn Örn Sigurðsson <kos@root.is>
2018-09-28 17:19:57 +0200

    More Slack helpers functions.

--- helpers/slack/slack.go
+++ helpers/slack/slack.go
@@ -2,15 +2,35 @@ package slack
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
-func Highlighted(userID, msg string) bool {
-	return strings.Contains(
+// IsCommand returns true if the message string is a direct command to the bot.
+func IsCommand(userID, msg string) bool {
+	return strings.HasPrefix(
 		msg,
 		fmt.Sprintf(
 			"<@%s>",
 			userID,
 		),
 	)
+}
+
+// ExtractCommand extracts the direct command to the bot from the message string.
+func ExtractCommand(userID, msg string) string {
+	re := regexp.MustCompile(
+		fmt.Sprintf(
+			"<@%s>\\W*",
+			userID,
+		),
+	)
+
+	return FormatCommand(re.ReplaceAllString(msg, ""))
+}
+
+// FormatCommand trims all whitespace from the command.
+// It also makes sure the whole command is lowercase.
+func FormatCommand(command string) string {
+	return strings.TrimSpace(strings.ToLower(command))
 }
--- helpers/slack/slack_test.go
+++ helpers/slack/slack_test.go
@@ -1,39 +1,121 @@
 package slack
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestHighlightedSuccess(t *testing.T) {
+func TestIsCommandSuccess(t *testing.T) {
 	userID := "ABCD"
 
-	highlightedTexts := []string{
+	commands := []string{
 		"<@ABCD> yoyo",
-		"blabla <@ABCD> yoyo",
-		"blabla yoyo <@ABCD>",
-		"<@ABDC> blabla yoyo <@ABCD>",
+		"<@ABCD>: blabla yoyo <@ABCD>",
+		"<@ABCD>yoyo",
+		"<@ABCD>:blabla yoyo <@ABCD>",
 	}
 
-	for _, text := range highlightedTexts {
-		if !Highlighted(userID, text) {
-			t.Error(text, "should be highlighted")
+	for _, cmd := range commands {
+		if !IsCommand(userID, cmd) {
+			t.Error(cmd, "should be a command")
 		}
 	}
 }
 
-func TestHighlightedFailure(t *testing.T) {
+func TestIsCommandFailure(t *testing.T) {
 	userID := "ABCD"
 
-	highlightedTexts := []string{
+	commands := []string{
 		"<ABCD> yoyo",
+		"<ABCD>: yoyo",
 		"blabla @ABCD yoyo",
 		"blabla yoyo <@ABCD",
 		"<@ABDC > blabla yoyo @ABCD>",
 	}
 
-	for _, text := range highlightedTexts {
-		if Highlighted(userID, text) {
-			t.Error(text, "shouldn't be highlighted")
+	for _, cmd := range commands {
+		if IsCommand(userID, cmd) {
+			t.Error(cmd, "shouldn't be a command")
+		}
+	}
+}
+
+func TestExtractCommandSuccess(t *testing.T) {
+	userID := "ABCD"
+
+	commands := []string{
+		"<@ABCD> list",
+		"<@ABCD> List",
+		"<@ABCD>: List",
+		"<@ABCD>List",
+		"<@ABCD>:List",
+		"<@ABCD> List <@ABCD>",
+		"<@ABCD> List <@ABCD> ",
+		"<@ABCD> List ",
+		"<@ABCD> List 	",
+	}
+
+	for _, cmd := range commands {
+		extractedCmd := ExtractCommand(userID, cmd)
+		if extractedCmd != "list" {
+			t.Error("expected", "list", "got", extractedCmd)
+		}
+	}
+}
+
+func TestExtractCommandFailure(t *testing.T) {
+	userID := "ABCD"
+
+	commands := []string{
+		"<@HAXX> list",
+		"<@HAXX> List",
+		"<@HAXX>: List",
+		"<@HAXX>List",
+		"<@HAXX>:List",
+		"<@HAXX> List <@HAXX>",
+		"<@HAXX> List <@HAXX> ",
+		"<@HAXX> List ",
+		"<@HAXX> List 	",
+	}
+
+	for _, cmd := range commands {
+		extractedCmd := ExtractCommand(userID, cmd)
+		expectedCmd := strings.TrimSpace(strings.ToLower(cmd))
+		if extractedCmd != expectedCmd {
+			t.Error("expected", expectedCmd, "got", extractedCmd)
+		}
+	}
+}
+
+func TestExtractCommandFailurePartRemoved(t *testing.T) {
+	userID := "ABCD"
+
+	commands := []string{
+		"<@HAXX> List <@ABCD>",
+		"<@HAXX> List <@ABCD> ",
+	}
+
+	expectedCmd := "<@haxx> list"
+	for _, cmd := range commands {
+		extractedCmd := ExtractCommand(userID, cmd)
+		if extractedCmd != expectedCmd {
+			t.Error("expected", expectedCmd, "got", extractedCmd)
+		}
+	}
+}
+
+func TestFormatCommand(t *testing.T) {
+	commands := []string{
+		"list",
+		"LIST",
+		" lIsT ",
+		" lIst 		",
+	}
+
+	for _, cmd := range commands {
+		formattedCmd := FormatCommand(cmd)
+		if formattedCmd != "list" {
+			t.Error("expected", "list", "got", formattedCmd)
 		}
 	}
 }
